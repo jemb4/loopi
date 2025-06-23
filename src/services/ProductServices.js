e426f4a53581a9a182c9a7eeb15bd3cd2ca1548b
Kenhya12 <paula.apse@gmail.com>
2025-06-23 12:44:19 +0200

    feature: incluir cambios en ProductServices.js

--- src/services/ProductServices.js
+++ src/services/ProductServices.js
@@ -1,6 +1,16 @@
 import { API_URL } from "./port.js";
 
 export class ProductServices {
+  static async getProduct() {
+  try {
+    const respuesta = await fetch(API_URL);
+      const datos = await respuesta.json();
+      console.log(datos);
+    } catch (error) {
+      console.error("Hubo un problema al obtener los datos", error);
+    }
+  };
+
   static async getSingleProduct(id) {
     try {
       const res = await fetch(`${API_URL}/${id}`);
